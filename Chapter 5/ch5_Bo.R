eec668a93f37faf9b218a679e0889afc89291134
changebo <booo.chang@gmail.com>
2014-10-27 16:05:10 -0700

    added Gaby's R script

--- Chapter 5/ch5_Bo.R
+++ Chapter 5/ch5_Bo.R
@@ -1,37 +1,66 @@
 library(ggplot2)
+X <- read.delim("http://www.stat.ubc.ca/~rickw/gapminderDataFiveYear.txt")
+
+# 'data.frame':  1704 obs. of  6 variables:
+#   $ country  : Factor w/ 142 levels "Afghanistan",..: 1 1 1 1 1 1 1 1 1 1 ...
+# $ year     : int  1952 1957 1962 1967 1972 1977 1982 1987 1992 1997 ...
+# $ pop      : num  8425333 9240934 10267083 11537966 13079460 ...
+# $ continent: Factor w/ 5 levels "Africa","Americas",..: 3 3 3 3 3 3 3 3 3 3 ...
+# $ lifeExp  : num  28.8 30.3 32 34 36.1 ...
+# $ gdpPercap: num  779 821 853 836 740 ...
 
 # 5.7 Maps
 qplot() + borders("state")
-qplot() + borders("state", "california")
-qplot() + borders("italy")
+qplot() + borders("world", "c")
+qplot() + xlim(-100, -60) + ylim(75, 83) + geom_polygon(aes(x=long, y=lat, group = group),
+                    data = subset(map_data("world")), alpha=0.5)
+qplot() + geom_path(aes(x=long, y=lat, group = group),
+                       data = subset(map_data("world"), region=="Canada"))
+qplot() + xlim(-100, -60) + ylim(75, 83) + geom_path(aes(x=long, y=lat, group = group),
+                    data = subset(map_data("world"), region=="Canada"))
+
+
 
 library(maps)
 data(us.cities)
+str(us.cities)
 big_cities <- subset(us.cities, pop > 500000)
 head(big_cities)
 p <- qplot(long, lat, data = big_cities)
 p
 p + borders("state", size = 0.5)
 
-ia <- map_data("county", "iowa")
-head(ia, 10)
-ggplot(ia, aes(long, lat)) + 
-  geom_polygon(aes(group = group, colour = subregion), fill = NA)
+ca <- map_data("county", "california")
+head(ca, 50)
+ggplot(ca, aes(long, lat)) + 
+  geom_polygon(aes(fill = subregion))
 
 
-# 5.9 Statistical summaries
-m1 <- ggplot(movies, aes(y=rating, x=year))
-m1 + stat_summary(fun.y = "median", colour = "red", geom = "line")
+# 5.8 & 5.9 Statistical summaries
+m1 <- ggplot(X, aes(y=lifeExp, x=factor(year))) + geom_point() 
+m1
+
+m1 + stat_summary(fun.y = mean, fun.ymin = min, fun.ymax = max,
+                  colour = "red", size=1)
+
+m1 + stat_summary(fun.y = mean, colour = "red", geom = "line")
+
+m2 <- m1 + stat_summary(fun.y = mean, aes(colour = "mean"), geom = "line") + 
+  stat_summary(fun.y = median, aes(colour = "median"), geom = "line")
+m2
+
+midm <- function(x) mean(x, trim = 0.25)
+m2 + stat_summary(fun.y = midm, aes(colour = "midm"), geom = "line")
 
 library(Hmisc)
 m1 + stat_summary(fun.data = "mean_cl_boot", colour = "red", geom = "smooth")
+m1 + stat_summary(fun.data = "mean_cl_boot", colour = "red", geom = "errorbar")
+# m1 + stat_summary(fun.data = "mean_cl_boot", colour = "red", aes(group = year),
+#                  geom = "crossbar")
+m1 + stat_summary(fun.data = "mean_cl_boot", colour = "red", geom = "pointrange")
+m1 + stat_summary(fun.data = "mean_cl_boot", colour = "red", geom = "crossbar")
 
 
-midm <- function(x) mean(x, trim = 0.5)
-
-m <- ggplot(movies, aes(y=log10(votes), x=round(rating)))
-m + stat_summary(aes(colour = "trimmed"), fun.y = midm, geom = "point") +
-  stat_summary(aes(colour = "raw"), fun.y = mean, geom = "point")
 
 iqr <- function(x, ...) {
   qs <- quantile(as.numeric(x), c(0.25, 0.75), na.rm = T)
@@ -39,10 +68,15 @@ iqr <- function(x, ...) {
   qs
 }
 
-m1 + stat_summary(fun.data = "iqr", geom="ribbon")
+m1 + stat_summary(fun.data = "iqr", geom="ribbon", 
+                  fill = "red", alpha = 0.4)
+m1 + stat_summary(fun.data = "iqr", geom="errorbar", color = "red")
+m1 + stat_summary(fun.data = "iqr", geom="linerange", color = "red")
+
 
 # 5.10 Annotation
 unemp <- qplot(date, unemploy, data=economics, geom="line")
+unemp
 xrng <- range(economics$date)
 yrng <- range(economics$unemploy)
 
@@ -61,4 +95,26 @@ unemp + geom_rect(aes(NULL, NULL, xmin = start, xmax = end, fill = party),
                   data = presidential[-(1:3), ], alpha = 0.3)
 
 
+
+# 5.11 Weighted data
+qplot(lifeExp, gdpPercap, data = X) +
+  geom_smooth()
+
+qplot(lifeExp, gdpPercap, data = X, size = pop, weight = pop) +
+  geom_smooth()
+
+qplot(lifeExp, gdpPercap, data = X, size = pop) +
+  geom_smooth(aes(weight = pop))
+
+qplot(lifeExp, data = X) 
+qplot(lifeExp, data = X, weight = pop) 
+
+
+# =====
+d <- qplot(factor(cyl), mpg, data=mtcars)
+d
+stat_sum_df <- function(fun, geom="crossbar", ...) {
+  stat_summary(fun.data=fun, colour="red", geom=geom, width=0.2, ...)
+}
+d + stat_sum_df("mean_cl_boot")
 
